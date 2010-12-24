2bd451a17d94c34790540d6294f3ff545afe53d7
Pedro P. Candel <pedro@joyent.com>
2010-12-24 10:57:19 +0100

    Moved mapi application from "/home/jill" to "/opt/smartdc". Jill user still with us due to some kind of romanticism.

--- zones/mapi/95-zone-datasets.sh
+++ zones/mapi/95-zone-datasets.sh
@@ -3,31 +3,38 @@ echo "95 configuring mapi datasets"
 # This needs to run after scmgit pkgsrc package has been installed:
 
 # mapi-data dataset name will remain the same always:
-zfs set mountpoint=/home/jill/mapi-data zones/mapi-data
+zfs set mountpoint=/opt/smartdc/mapi-data zones/mapi-data
 # mapi-app-ISO_DATE dataset name will change:
 STAMP=$(cat /root/mapi-app-timestamp)
-zfs set mountpoint=/home/jill/mapi "zones/mapi-app-$STAMP"
+zfs set mountpoint=/opt/smartdc/mapi "zones/mapi-app-$STAMP"
 # Get git revision:
-cd /home/jill/mapi-repo
+cd /opt/smartdc/mapi-repo
 REVISION=$(/opt/local/bin/git rev-parse --verify HEAD)
 # Export complete repo into mapi:
-cd /home/jill/mapi-repo
-/opt/local/bin/git checkout-index -f -a --prefix=/home/jill/mapi/
+cd /opt/smartdc/mapi-repo
+/opt/local/bin/git checkout-index -f -a --prefix=/opt/smartdc/mapi/
 # Export only config into mapi-data:
-cd /home/jill/mapi-repo
-/opt/local/bin/git checkout-index -f --prefix=/home/jill/mapi-data/ config/
+cd /opt/smartdc/mapi-repo
+/opt/local/bin/git checkout-index -f --prefix=/opt/smartdc/mapi-data/ config/
 # Create some directories into mapi-data
-mkdir -p /home/jill/mapi-data/log
-mkdir -p /home/jill/mapi-data/tmp/pids
+mkdir -p /opt/smartdc/mapi-data/log
+mkdir -p /opt/smartdc/mapi-data/tmp/pids
 # Remove and symlink directories:
-mv /home/jill/mapi/config /home/jill/mapi-data/config
-rm -Rf /home/jill/mapi/log
-rm -Rf /home/jill/mapi/tmp
-rm -Rf /home/jill/mapi/config
-ln -s /home/jill/mapi-data/log /home/jill/mapi/log
-ln -s /home/jill/mapi-data/tmp /home/jill/mapi/tmp
-ln -s /home/jill/mapi-data/config /home/jill/mapi/config
+mv /opt/smartdc/mapi/config /opt/smartdc/mapi-data/config
+rm -Rf /opt/smartdc/mapi/log
+rm -Rf /opt/smartdc/mapi/tmp
+rm -Rf /opt/smartdc/mapi/config
+ln -s /opt/smartdc/mapi-data/log /opt/smartdc/mapi/log
+ln -s /opt/smartdc/mapi-data/tmp /opt/smartdc/mapi/tmp
+ln -s /opt/smartdc/mapi-data/config /opt/smartdc/mapi/config
 # Save REVISION:
-echo "${REVISION}">/home/jill/mapi-data/REVISION
-echo "${REVISION}">/home/jill/mapi/REVISION
-# TODO: delete mapi-repo
+echo "${REVISION}">/opt/smartdc/mapi-data/REVISION
+echo "${REVISION}">/opt/smartdc/mapi/REVISION
+# Save VERSION (Updates based on this):
+APP_VERSION=$(/opt/local/bin/git describe --tags)
+echo "${APP_VERSION}">/opt/smartdc/mapi-data/VERSION
+echo "${APP_VERSION}">/opt/smartdc/mapi/VERSION
+# Cleanup build products:
+cd /root/
+rm -Rf /opt/smartdc/mapi-repo
+rm /root/mapi-app-timestamp
