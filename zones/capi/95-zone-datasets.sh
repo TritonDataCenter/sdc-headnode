a9859468e873216d48fa17b125997c0c1870dd00
Scott McWhirter <scott.mcwhirter@joyent.com>
2010-12-27 15:43:14 -0800

    Add initial copy of MAPI and CAPI-ify it

--- zones/capi/95-zone-datasets.sh
+++ zones/capi/95-zone-datasets.sh
@@ -1,49 +1,49 @@
-echo "95 configuring mapi datasets"
+echo "95 configuring capi datasets"
 
 # This needs to run after scmgit pkgsrc package has been installed:
 
-# mapi-data dataset name will remain the same always:
-zfs set mountpoint=/opt/smartdc/mapi-data zones/mapi-data
-# mapi-app-ISO_DATE dataset name will change:
-STAMP=$(cat /root/mapi-app-timestamp)
-zfs set mountpoint=/opt/smartdc/mapi "zones/mapi-app-$STAMP"
+# capi-data dataset name will remain the same always:
+zfs set mountpoint=/opt/smartdc/capi-data zones/capi-data
+# capi-app-ISO_DATE dataset name will change:
+STAMP=$(cat /root/capi-app-timestamp)
+zfs set mountpoint=/opt/smartdc/capi "zones/capi-app-$STAMP"
 # Get git revision:
-cd /opt/smartdc/mapi-repo
+cd /opt/smartdc/capi-repo
 REVISION=$(/opt/local/bin/git rev-parse --verify HEAD)
-# Export complete repo into mapi:
-cd /opt/smartdc/mapi-repo
-/opt/local/bin/git checkout-index -f -a --prefix=/opt/smartdc/mapi/
-# Export only config into mapi-data:
-cd /opt/smartdc/mapi-repo
-/opt/local/bin/git checkout-index -f --prefix=/opt/smartdc/mapi-data/ config/
-# Create some directories into mapi-data
-mkdir -p /opt/smartdc/mapi-data/log
-mkdir -p /opt/smartdc/mapi-data/tmp/pids
+# Export complete repo into capi:
+cd /opt/smartdc/capi-repo
+/opt/local/bin/git checkout-index -f -a --prefix=/opt/smartdc/capi/
+# Export only config into capi-data:
+cd /opt/smartdc/capi-repo
+/opt/local/bin/git checkout-index -f --prefix=/opt/smartdc/capi-data/ config/
+# Create some directories into capi-data
+mkdir -p /opt/smartdc/capi-data/log
+mkdir -p /opt/smartdc/capi-data/tmp/pids
 # Remove and symlink directories:
-mv /opt/smartdc/mapi/config /opt/smartdc/mapi-data/config
-rm -Rf /opt/smartdc/mapi/log
-rm -Rf /opt/smartdc/mapi/tmp
-rm -Rf /opt/smartdc/mapi/config
-ln -s /opt/smartdc/mapi-data/log /opt/smartdc/mapi/log
-ln -s /opt/smartdc/mapi-data/tmp /opt/smartdc/mapi/tmp
-ln -s /opt/smartdc/mapi-data/config /opt/smartdc/mapi/config
+mv /opt/smartdc/capi/config /opt/smartdc/capi-data/config
+rm -Rf /opt/smartdc/capi/log
+rm -Rf /opt/smartdc/capi/tmp
+rm -Rf /opt/smartdc/capi/config
+ln -s /opt/smartdc/capi-data/log /opt/smartdc/capi/log
+ln -s /opt/smartdc/capi-data/tmp /opt/smartdc/capi/tmp
+ln -s /opt/smartdc/capi-data/config /opt/smartdc/capi/config
 # Save REVISION:
-echo "${REVISION}">/opt/smartdc/mapi-data/REVISION
-echo "${REVISION}">/opt/smartdc/mapi/REVISION
+echo "${REVISION}">/opt/smartdc/capi-data/REVISION
+echo "${REVISION}">/opt/smartdc/capi/REVISION
 # Save VERSION (Updates based on this):
 APP_VERSION=$(/opt/local/bin/git describe --tags)
-echo "${APP_VERSION}">/opt/smartdc/mapi-data/VERSION
-echo "${APP_VERSION}">/opt/smartdc/mapi/VERSION
+echo "${APP_VERSION}">/opt/smartdc/capi-data/VERSION
+echo "${APP_VERSION}">/opt/smartdc/capi/VERSION
 # Cleanup build products:
 cd /root/
-rm -Rf /opt/smartdc/mapi-repo
-rm /root/mapi-app-timestamp
+rm -Rf /opt/smartdc/capi-repo
+rm /root/capi-app-timestamp
 
 # Adding dataset based update service for the app:
-cat >"/opt/smartdc/mapi-data/mapi-update-service.sh" <<UPDATE
+cat >"/opt/smartdc/capi-data/capi-update-service.sh" <<UPDATE
 #!/usr/bin/bash
 
-APP_NAME='mapi'
+APP_NAME='capi'
 
 APP_VERSION=\$(cat /opt/smartdc/\$APP_NAME/VERSION)
 DATA_VERSION=\$(cat /opt/smartdc/\$APP_NAME-data/VERSION)
@@ -59,4 +59,4 @@ exit 0
 
 UPDATE
 
-chmod +x /opt/smartdc/mapi-data/mapi-update-service.sh
+chmod +x /opt/smartdc/capi-data/capi-update-service.sh
