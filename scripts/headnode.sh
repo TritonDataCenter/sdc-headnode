05d491507c1d345fa8be35ad3d057ec1f3f81d6a
Pedro P. Candel <pedro@joyent.com>
2011-01-13 19:38:08 +0100

    HEAD-23: If we copy after the data has been created, it's useless

--- scripts/headnode.sh
+++ scripts/headnode.sh
@@ -280,11 +280,6 @@ if [ -n "${CREATEDZONES}" ]; then
             fi
         fi
 
-        # copy headnode info into zone if we're MAPI
-        if [[ "${zone}" == "mapi" ]] && [[ -d "/zones/mapi/root/opt/smartdc/mapi-data" ]]; then
-            sysinfo > /zones/mapi/root/opt/smartdc/mapi-data/headnode-sysinfo.json
-        fi
-
         # copy dhcpd configuration into zone if we're DHCPD
         if [[ "${zone}" == "dhcpd" ]] && [[ -d "/zones/dhcpd/root/etc" ]]; then
             ${USB_PATH}/zones/dhcpd/tools/dhcpconfig
