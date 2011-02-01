b19bc468c254f37069e1f54ae253bbfdb9635a7d
John Sonnenschein <johns@joyent.com>
2011-02-01 12:12:15 -0800

    [HEAD-142] would like a mount-usb script

--- scripts/mount-image.sh
+++ scripts/mount-image.sh
@@ -18,19 +18,9 @@ if [[ ! -d $mnt ]]; then
 	mkdir $mnt || fatal "could not make $mnt"
 fi
 
-USBKEYS=`/usr/bin/disklist -a`
-for key in ${USBKEYS}; do
-    if [[ `/usr/sbin/fstyp /dev/dsk/${key}p0:1` == 'pcfs' ]]; then
-        /usr/sbin/mount -F pcfs -o foldcase /dev/dsk/${key}p0:1 ${usbmnt};
-        if [[ $? == "0" ]]; then
-            if [[ ! -f ${usbmnt}/.joyliveusb ]]; then
-                /usr/sbin/umount ${usbmnt};
-            else
-                break;
-            fi
-        fi
-    fi
-done
+usbcp="$(svcprop -p 'joyentfs/usb_copy_path' svc:/system/filesystem/smartdc:default)"
+
+mount | grep "^${usbmnt}" >/dev/null 2>&1 || bash $usbcp/scripts/mount-usb.sh
 
 mount | grep "^${usbmnt}" >/dev/null 2>&1 || fatal "${usbmnt} is not mounted"
 
---
+++ scripts/mount-usb.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+usbmnt="/mnt/$(svcprop -p 'joyentfs/usb_mountpoint' svc:/system/filesystem/smartdc:default)"
+
+USBKEYS=`/usr/bin/disklist -a`
+for key in ${USBKEYS}; do
+    if [[ `/usr/sbin/fstyp /dev/dsk/${key}p0:1` == 'pcfs' ]]; then
+        /usr/sbin/mount -F pcfs -o foldcase /dev/dsk/${key}p0:1 ${usbmnt};
+        if [[ $? == "0" ]]; then
+            if [[ ! -f ${usbmnt}/.joyliveusb ]]; then
+                /usr/sbin/umount ${usbmnt};
+            else
+                break;
+            fi
+        fi
+    fi
+done
+
+mount | grep "^${usbmnt}" >/dev/null 2>&1 || fatal "${usbmnt} is not mounted"
+
+
