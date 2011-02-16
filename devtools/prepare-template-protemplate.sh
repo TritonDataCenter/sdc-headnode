192d06f5d50e9166e0e26e22f4a9e2d23a45dff1
Trent Mick <trentm@gmail.com>
2011-02-16 10:36:14 -0800

    use the assets zone on the headnode to get these bits now
    Mostly these scripts are vestigial now that Josh added loading of the
    datasets on compute node "joysetup.sh" for entries in the
    `compute_node_initial_datasets` config var. Can still be useful
    for subsequent running.

--- devtools/prepare-template-protemplate.sh
+++ devtools/prepare-template-protemplate.sh
@@ -10,11 +10,12 @@ set -o errexit      # crash on errors
 set -o pipefail     # crash on errors in pipelines
 #set -o xtrace       # debugging output
 
+source /lib/sdc/config.sh
+load_sdc_config
 
 DATASET_NAME=protemplate-2.5.2
 DATASET_BASE=$(echo $DATASET_NAME | awk -F'-' '{print $1}')
-ASSETS_JOYENT_US_IP=$(dig @8.8.8.8 assets.joyent.us +short)
-DATASET_RELEASES="https://guest:GrojhykMid@${ASSETS_JOYENT_US_IP}/templates"
+DATASET_RELEASES="http://${CONFIG_assets_admin_ip}/datasets"
 
 
 # Get the dataset.
@@ -24,7 +25,7 @@ if [[ `zfs list -H -o name zones/$DATASET_NAME 2>/dev/null` != "zones/$DATASET_N
     dataset_path=$dataset_usbkey_path
   else
     dataset_url=${DATASET_RELEASES}/${DATASET_NAME}.zfs.bz2
-    echo "Downloading '$DATASET_NAME' from assets.joyent.us."
+    echo "==> Downloading ${dataset_url}"
     (cd /tmp && curl --progress-bar -k -O ${dataset_url})
     dataset_path=/tmp/${DATASET_NAME}.zfs.bz2
   fi
