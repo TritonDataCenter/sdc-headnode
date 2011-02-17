1fb91af62fb27528a58669ca45f045e468c014ef
Jerry Jelinek <jerry.jelinek@joyent.com>
2011-02-17 10:33:01 -0700

    OS-247 add dhcp_server property to dhcp zone OS-223 zone's should create vnics as needed instead of requiring them to be set up in advance

--- scripts/headnode.sh
+++ scripts/headnode.sh
@@ -197,6 +197,13 @@ for zone in $ALLZONES; do
             zone_external_vlan_opts="-v ${zone_external_vlan}"
         fi
 
+	zone_dhcp_server_enable=""
+        zone_dhcp_server=$(. ${USB_COPY}/config
+            eval "echo \${${zone}_dhcp_server}"
+        )
+        [[ -n ${zone_dhcp_server} ]] &&
+	    zone_dhcp_server_enable="add property (name=dhcp_server,value=1)"
+
         echo -n "creating zone ${zone}... " >>/dev/console
         zonecfg -z ${zone} -f ${src}/config
 
@@ -219,32 +226,10 @@ for zone in $ALLZONES; do
             fi
         )
 
-        zonecfg -z ${zone} "add net; set physical=${zone}0; set vlan-id=0; set global-nic=admin; end; exit"
+        zonecfg -z ${zone} "add net; set physical=${zone}0; set vlan-id=0; set global-nic=admin; ${zone_dhcp_server_enable}; end; exit"
         if [[ -n "${zone_external_ip}" ]] && [[ "${zone_external_ip}" != "${zone_admin_ip}" ]]; then
-           zonecfg -z ${zone} "add net; set physical=${zone}1; set vlan-id=${zone_external_vlan}; set global-nic=external; end; exit"
+           zonecfg -z ${zone} "add net; set physical=${zone}1; set vlan-id=${zone_external_vlan}; set global-nic=external; ${zone_dhcp_server_enable}; end; exit"
         fi
-
-        dladm show-phys -m -p -o link,address | \
-          sed 's/:/\ /;s/\\//g' | while read iface mac; do
-            if [[ ${mac} == ${admin_nic} ]]; then
-                dladm create-vnic -l ${iface} ${zone}0
-                admin_vnic_mac=$(dladm show-vnic -p -o MACADDRESS ${zone}0)
-
-                [[ -n ${admin_vnic_mac} ]] && zonecfg -z ${zone} \
-		    "select net physical=${zone}0; set mac-addr=${admin_vnic_mac}; end; exit"
-            fi
-
-            # if we have a PUBLIC_IP too, we need a second NIC
-            if [[ ${mac} == ${external_nic} ]] && \
-		[[ -n "${zone_external_ip}" ]] && \
-		[[ "${zone_external_ip}" != "${zone_admin_ip}" ]]; then
-                dladm create-vnic -l ${iface} ${zone_external_vlan_opts} ${zone}1
-                external_vnic_mac=$(dladm show-vnic -p -o MACADDRESS ${zone}1)
-
-                [[ -n ${external_vnic_mac} ]] && zonecfg -z ${zone} \
-		    "select net physical=${zone}1; set mac-addr=${external_vnic_mac}; end; exit"
-            fi
-        done
 
         zoneadm -z ${zone} install -t ${LATESTTEMPLATE}
 
