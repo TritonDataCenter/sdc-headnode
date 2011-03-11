933c0849e6cdcc272a4c08ffff824cb9d6411cd7
Pedro P. Candel <pedro@joyent.com>
2011-03-11 19:23:14 +0100

    [HEAD-300] configure script for portal + shared code with zoneinit-finalize

---
+++ zones/portal/fs.root/root/opt/smartdc/bin/configure.sh
@@ -0,0 +1,109 @@
+# This script can be sourced either as part of zoneinit-finalize
+# or directly from head-node global zone, when reconfiguring the zone
+# for whatever the reason using /opt/smartdc/bin/configure
+
+# node request package runs fine in npm, but here, we need to explicitly setup an index.js
+# to map it to something plain node understands
+if [ ! -e /opt/smartdc/portal/deps/request/index.js ]; then
+    (cd /opt/smartdc/portal/deps/request; ln -s main.js index.js)
+fi
+
+# Update the config with the correct values.
+cat > /opt/smartdc/portal/config.js <<HERE
+exports.config = {
+  displayCouponField : false,
+  publicApiUrl : "http://${PUBLIC_API_PRIVATE_IP}:8080/v1",
+  privateCAUrl : "http://${MAPI_API_PRIVATE_IP}:80",
+  privateCAPIUrl : "http://${CAPI_API_PRIVATE_IP}:8080",
+  CAPIuser : "admin",
+  CAPIpassword : "tot@ls3crit",
+  CAPIMetaCAKey : "portal-coal",
+  CAPIMetaCABlessed : "blessed-instrumentation",
+  defaultCAParams : { module : "node", stat : "httpd_ops", decomposition : "raddr" },
+  defaultCAChoices : [
+    { label : "HTTP server operations", params : { module : "node", stat : "httpd_ops", decomposition : "raddr" }},
+    { label : "HTTP client operations", params : { module : "node", stat : "httpc_ops", decomposition : "raddr" }},
+    { label : "Socket read/write operations", params : { module : "node", stat : "socket_ops", decomposition : "raddr" }},
+  ],
+  listenIp : "${PRIVATE_IP}"
+}
+HERE
+
+# We need to override nginx.conf on reconfigure, and it's safe to do during setup:
+echo "Creating nginx configuration file"
+cat >/opt/local/etc/nginx/nginx.conf <<NGINX
+user www www;
+worker_processes 1;
+error_log /var/log/nginx/error.log;
+#pid /var/spool/nginx/nginx.pid;
+
+events {
+    worker_connections 1024;
+    use /dev/poll; # important on Solaris
+}
+
+http {
+    include /opt/local/etc/nginx/mime.types;
+    default_type application/octet-stream;
+    log_format main '\$remote_addr - \$remote_user [\$time_local] \$request '
+                    '"\$status" \$body_bytes_sent "\$http_referer" '
+                    '"\$http_user_agent" "\$http_x_forwarded_for"';
+    access_log /var/log/nginx/access.log main;
+    sendfile off; # important on Solaris
+    keepalive_timeout 60;
+    server_tokens off;
+
+    upstream portal {
+        server ${PRIVATE_IP}:4000;
+    }
+    
+    server {
+        listen 80;
+        rewrite ^(.*) https://\$host\$1 permanent;
+    }
+
+    server {
+        listen 443;
+
+        # Self-signed is okay. Production Zeus will handle the real no.de certs.
+        ssl on;
+        ssl_certificate /opt/local/etc/openssl/private/selfsigned.pem;
+        ssl_certificate_key /opt/local/etc/openssl/private/selfsigned.pem;
+        ssl_prefer_server_ciphers on;
+
+        location / {
+            root /opt/smartdc/portal/public;
+            proxy_set_header X-Real-IP  \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header Host \$http_host;
+            proxy_redirect off;
+
+            ## Serve static files
+            #if (-f \$request_filename) {
+            #    break;
+            #}
+
+            proxy_pass http://portal;
+            break;
+        }
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+            root share/examples/nginx/html;
+        }
+    }
+}
+
+NGINX
+
+# Setup and configure nginx
+if [[ -z $(/usr/bin/svcs -a|grep nginx) ]]; then
+  echo "Importing nginx service"
+  /usr/sbin/svccfg import /opt/local/share/smf/manifest/nginx.xml
+  /usr/sbin/svcadm enable -s nginx
+else
+  echo "Restarting nginx service"
+  /usr/sbin/svcadm disable -s nginx
+  /usr/sbin/svcadm enable -s nginx
+fi
+
