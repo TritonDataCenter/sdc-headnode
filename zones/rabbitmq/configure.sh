ddd472967c58860c15fcbe8fbbdd3f2d1cf08276
Josh Wilsdon <jwilsdon@joyent.com>
2011-03-24 12:52:00 -0700

    [HEAD-344] first attempt at fixing rabbitmq timing problems.

--- zones/rabbitmq/configure.sh
+++ zones/rabbitmq/configure.sh
@@ -23,9 +23,21 @@ esac
 # Make sure we can actually talk to RabbitMQ
 loops=0
 while ! su - rabbitmq -c \
-    "/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq -q list_users" \
+    "/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq -q list_vhosts" \
     >/dev/null 2>&1; do
 
+    loops=$((${loops} + 1))
+    if [[ ${loops} -gt 30 ]]; then
+        echo "Rabbitmq timed out."
+        exit 1
+    fi
+    sleep 1
+done
+
+# Even after we can list the vhosts, rabbit is still possibly not up!
+# Check that we're also able to hit the overview page.
+loops=0
+while ! curl -i http://localhost:55672/api/overview; do
     loops=$((${loops} + 1))
     if [[ ${loops} -gt 30 ]]; then
         echo "Rabbitmq timed out."
@@ -43,11 +55,13 @@ for user in $(su - rabbitmq -c \
         "/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq delete_user ${user}"
 done
 
-# Make sure we have the '/' vhost
-if ! (su - rabbitmq -c '/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq -q list_vhosts' \
-    | grep "^\/$"); then
+# Double check that we really don't have any users
+num_users=$(su - rabbitmq -c \
+    "/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq -q list_users" | wc -l)
 
-    su - rabbitmq -c '/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq add_vhost "/"'
+if [[ ${num_users} != "0" ]]; then
+    echo "Failed to remove users!"
+    exit 1
 fi
 
 # Add user, or if none specified: use guest
@@ -61,5 +75,6 @@ fi
 
 echo "Adding user ${amqp_user} and setting permissions"
 su - rabbitmq -c "/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq add_user ${amqp_user} ${amqp_pass}"
+su - rabbitmq -c "/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq set_admin ${amqp_user}"
 su - rabbitmq -c "/opt/local/sbin/rabbitmqctl -n rabbit@rabbitmq set_permissions -p / ${amqp_user} \".*\" \".*\" \".*\""
 
