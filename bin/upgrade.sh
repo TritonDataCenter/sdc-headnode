e14d317a352268d5bb8a8e05e48dcfd56ccef026
Josh Wilsdon <jwilsdon@joyent.com>
2011-03-30 10:31:08 -0700

    [HEAD-353] fix handling of datasets, and now make sure new datasets are imported when recreating zones.

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -2,6 +2,11 @@
 #
 # Copyright (c) 2011 Joyent Inc., All rights reserved.
 #
+# SUMMARY
+#
+# This is an example upgrade.sh script which shows how one will be built for an
+# actual SDC upgrade.
+#
 
 BASH_XTRACEFD=4
 set -o xtrace
@@ -33,9 +38,16 @@ if [[ ${SYSINFO_Bootparam_headnode} != "true" \
     || $(uname -s) != "SunOS" \
     || -z ${SYSINFO_Live_Image} ]]; then
 
-    echo "This can only be run on a SmartOS headnode."
-    exit 1
+    fatal "this can only be run on a SmartOS headnode."
 fi
+
+function fatal
+{
+    msg=$1
+
+    echo "--> FATAL: ${msg}"
+    exit 1
+}
 
 function cleanup
 {
@@ -58,12 +70,15 @@ function check_versions
 {
     new_version=$(cat ${ROOT}/VERSION)
 
-    existing_version=$(cat ${usbmnt}/version)
+    existing_version=$(cat ${usbmnt}/version 2>/dev/null)
     if [[ -z ${existing_version} ]]; then
+        echo "--> Warning: unable to find version file in ${usbmnt}, assuming build is ancient."
         existing_version="ancient"
     fi
 
     # TODO: check system / version to ensure it's possible to apply this update.
+    #
+    # This needs to be filled in manually as part of an actual SDC upgrade.
 
     echo "==> Upgrading from ${existing_version} to ${new_version}"
 }
@@ -73,17 +88,20 @@ function backup_usbkey
     backup_dir=${usbcpy}/backup/${existing_version}.$(date -u +%Y%m%dT%H%M%SZ)
 
     if [[ -d ${backup_dir} ]]; then
-        echo "FATAL: unable to create back dir ${backup_dir}"
-        exit 1
+        fatal "unable to create back dir ${backup_dir}"
     fi
     mkdir -p ${backup_dir}/usbkey
     mkdir -p ${backup_dir}/zones
 
     echo "==> Creating backup in ${backup_dir}"
 
+    # touch these, just to make sure they exist (in case of ancient build)
+    touch ${usbmnt}/datasets/smartos.uuid
+    touch ${usbmnt}/datasets/smartos.filename
+
     (cd ${usbmnt} && gtar -cf - \
         boot/grub/menu.lst.tmpl \
-        datasets \
+        datasets/smartos.{uuid,filename} \
         rc \
         scripts \
         ur-scripts \
@@ -101,6 +119,28 @@ function upgrade_usbkey
 
         # XXX (this is the point where we'd fix the config in /mnt/usbkey/config)
         (cd ${usbmnt} && rsync -a --exclude private --exclude os * ${usbcpy})
+    fi
+}
+
+function import_datasets
+{
+    ds_uuid=$(cat ${usbmnt}/datasets/smartos.uuid)
+    ds_file=$(cat ${usbmnt}/datasets/smartos.filename)
+
+    if [[ -z ${ds_uuid} ]]; then
+        fatal "no uuid set in ${usbmnt}/datasets/smartos.uuid"
+    else
+        echo "==> Ensuring ${ds_uuid} is imported."
+        if [[ -z $(zfs list | grep "^zones/e2abe3f6-5668-11e0-bab1-07a4d450d804") ]]; then
+            # not already imported
+            if [[ -f ${usbmnt}/datasets/${ds_file} ]]; then
+                bzcat ${usbmnt}/datasets/${ds_file} \
+                    | zfs recv zones/${ds_uuid} \
+                    || fatal "unable to import ${ds_uuid}"
+            else
+                fatal "unable to import ${ds_uuid} (${ds_file} doesn't exist)"
+            fi
+        fi
     fi
 }
 
@@ -122,6 +162,16 @@ function recreate_zones
     if [[ -x /opt/smartdc/agents/bin/setup-npm-user ]]; then
         /opt/smartdc/agents/bin/setup-npm-user joyent joyent atropos@joyent.com
     fi
+}
+
+function backup_npm_registry
+{
+    echo "==> Would backup npm registry from atropos"
+}
+
+function restore_npm_registry
+{
+    echo "==> Would restore npm registry to atropos"
 }
 
 function upgrade_zones
@@ -184,12 +234,12 @@ backup_usbkey
 upgrade_usbkey
 trap cleanup EXIT
 
-# TODO: import new headnode dataset if there's one.
-#
-# if smartos.uuid not imported, import smartos.filename
-#
+# import new headnode dataset if there's one (used for new headnode zones)
+import_datasets
 
+backup_npm_registry
 recreate_zones
+restore_npm_registry
 upgrade_zones
 reenable_agents
 
