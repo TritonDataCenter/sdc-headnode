391fda972f97e2515231fef5330c3888b9e6518c
Jerry Jelinek <jerry.jelinek@joyent.com>
2011-05-19 09:00:46 -0600

    HEAD-249 destroy_zone needs to wait until zone has shutdown before uninstalling

--- scripts/destroy-zone.sh
+++ scripts/destroy-zone.sh
@@ -20,6 +20,25 @@ fi
 
 echo -n "Destroying '${zone}':"
 zoneadm -z ${zone} halt || /bin/true
+
+state=`zoneadm -z ${zone} list -p | cut -d: -f3`
+while [ "$state" != "installed" ]; do
+	sleep 5
+	state=`zoneadm -z ${zone} list -p | cut -d: -f3`
+	if [ "$state" != "installed" ]; then
+		echo
+		echo "Waiting for zone to shutdown, state: $state"
+
+		for i in `mount -p | nawk -v zname=${zone} '{
+			if (index($3, zname) != 0)
+				print $3
+		}'`; do
+			echo "check file system: $i"
+			fuser $i
+		done
+	fi
+done
+
 echo -n " halt"
 zoneadm -z ${zone} uninstall -F || /bin/true
 echo -n ", uninstall"
