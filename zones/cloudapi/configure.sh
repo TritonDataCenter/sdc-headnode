b5ad65bab86be6e2e808c26ad0ad9335ae8a90be
Mark Cavage <mark.cavage@joyent.com>
2011-06-06 18:29:37 -0700

    [HEAD-444] Add support for /datacenters and default_dataset to cloudapi zone config

--- zones/cloudapi/configure.sh
+++ zones/cloudapi/configure.sh
@@ -37,6 +37,7 @@ cat > /opt/smartdc/cloudapi/cfg/config.json <<HERE
 {
   "siteName": "${CLOUDAPI_EXTERNAL_URL}",
   "port": 80,
+  "datacenter": "${DATACENTER_NAME}",
   "logLevel": 4,
   "capi": {
     "uri": "${CAPI_URL}",
@@ -65,21 +66,12 @@ cat > /opt/smartdc/cloudapi/cfg/config.json <<HERE
     "username": "${MAPI_HTTP_ADMIN_USER}",
     "password": "${MAPI_HTTP_ADMIN_PW}"
   },
-  "datasets": {
-    "smartos": {
-      "1.3.13": "63ce06d8-7ae7-11e0-b0df-1fcf8f45c5d5",
-      "_default": "1.3.13"
-    },
-    "nodejs": {
-      "1.1.4": "41da9c2e-7175-11e0-bb9f-536983f41cd8",
-      "_default": "1.1.4"
-    },
-    "ubuntu": {
-      "10.04.2.1": "b66fb52a-6a8a-11e0-94cd-b347300c5a06",
-      "_default": "10.04.2.1"
-    },
-    "_default": "smartos"
+  "default_limits": {
+    "smartos": 1,
+    "nodejs": 1,
+    "ubuntu": 1
   },
+  "default_dataset": "63ce06d8-7ae7-11e0-b0df-1fcf8f45c5d5",
   "default_network": "external",
   "default_package": "regular_128",
   "datacenters": {
