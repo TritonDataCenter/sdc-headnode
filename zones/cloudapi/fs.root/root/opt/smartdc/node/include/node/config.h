6f85a01737dbc1f5a825a6cbd92a245b58c4bebc
Mark Cavage <mark.cavage@joyent.com>
2011-06-10 12:44:22 -0700

    Add github.com/mcavage/node (v0.4.9-pre w/RSA fixes)

---
+++ zones/cloudapi/fs.root/root/opt/smartdc/node/include/node/config.h
@@ -0,0 +1,32 @@
+/* Configuration header created by Waf - do not edit */
+#ifndef _CONFIG_H_WAF
+#define _CONFIG_H_WAF
+
+#define HAVE_SSL_LIBRARY_INIT 1
+#define HAVE_OPENSSL_CRYPTO_H 1
+#define HAVE_PTHREAD_CREATE 1
+#define HAVE_PTHREAD_ATFORK 1
+/* #undef HAVE_FUTIMES */
+/* #undef HAVE_READAHEAD */
+#define HAVE_FDATASYNC 1
+#define HAVE_PREADWRITE 1
+/* #undef HAVE_SENDFILE */
+/* #undef HAVE_SYNC_FILE_RANGE */
+/* #undef HAVE_SYS_INOTIFY_H */
+/* #undef HAVE_SYS_EPOLL_H */
+#define HAVE_PORT_H 1
+#define HAVE_PORT_CREATE 1
+#define HAVE_POLL_H 1
+#define HAVE_POLL 1
+/* #undef HAVE_SYS_EVENT_H */
+#define HAVE_SYS_QUEUE_H 1
+/* #undef HAVE_KQUEUE */
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_SELECT 1
+/* #undef HAVE_SYS_EVENTFD_H */
+/* #undef HAVE_CLOCK_SYSCALL */
+#define HAVE_CLOCK_GETTIME 1
+#define HAVE_NANOSLEEP 1
+#define HAVE_CEIL 1
+#define HAVE_CONFIG_H 1
+#endif /* _CONFIG_H_WAF */
