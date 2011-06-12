9d4ddf8387d6a747db39b8e8c4fb3671d266d6bf
Mark Cavage <mark.cavage@joyent.com>
2011-06-11 21:36:20 -0700

    [HEAD-448] Add throttling config to cloudapi

--- zones/cloudapi/configure.sh
+++ zones/cloudapi/configure.sh
@@ -2,37 +2,6 @@
 # or directly from head-node global zone, when reconfiguring the zone
 # for whatever the reason using /opt/smartdc/bin/configure
 
-# v1
-cat > /opt/smartdc/v1/config/production.json <<HERE
-{
-  "name": "Joyent Public API",
-  "url": "http://${PUBLIC_IP}/v1",
-  "capi": {
-    "url": "${CAPI_URL}",
-    "username": "${CAPI_HTTP_ADMIN_USER}",
-    "password": "${CAPI_HTTP_ADMIN_PW}"
-  },
-  "mapi": {
-    "${DEFAULT_DATACENTER}": {
-      "url": "${MAPI_URL}",
-      "username": "${MAPI_HTTP_ADMIN_USER}",
-      "password": "${MAPI_HTTP_ADMIN_PW}",
-      "resources": {
-        "nodejs": {
-          "repo": true,
-          "domain": "*.no.de",
-          "ram": [128, 256, 512, 1024, 2048, 4096]
-        },
-        "smartos": {
-          "ram": [128, 256, 512, 1024, 2048, 4096]
-        }
-      }
-    }
-  }
-}
-HERE
-
-# v2
 cat > /opt/smartdc/cloudapi/cfg/config.json <<HERE
 {
   "siteName": "${CLOUDAPI_EXTERNAL_URL}",
@@ -66,16 +35,100 @@ cat > /opt/smartdc/cloudapi/cfg/config.json <<HERE
     "username": "${MAPI_HTTP_ADMIN_USER}",
     "password": "${MAPI_HTTP_ADMIN_PW}"
   },
+  "rabbitmq": {
+    "host": "${RABBIT_IP}",
+    "user": "guest",
+    "password": "guest",
+    "vhost": "/"
+  },
   "default_limits": {
     "smartos": 1,
     "nodejs": 1,
     "ubuntu": 1
   },
   "default_dataset": "63ce06d8-7ae7-11e0-b0df-1fcf8f45c5d5",
-  "default_network": "external",
-  "default_package": "regular_128",
   "datacenters": {
     "${DATACENTER_NAME}": "${CLOUDAPI_EXTERNAL_URL}"
+  },
+  "ipThrottles": {
+    "all": {
+      "ip": true,
+      "burst": 9,
+      "rate": 3,
+      "overrides": {
+        "${PORTAL_ADMIN_IP}": {
+          "burst": 0,
+          "rate": 0
+        }
+      }
+    },
+    "analytics": {
+      "ip": true,
+      "burst": 100,
+      "rate": 10,
+      "overrides": {
+        "${PORTAL_ADMIN_IP}": {
+          "burst": 0,
+          "rate": 0
+        }
+      }
+    }
+  },
+  "userThrottles": {
+    "all": {
+      "username": true,
+      "burst": 30,
+      "rate": 10,
+      "overrides": {
+        "${CAPI_ADMIN_LOGIN}": {
+          "burst": 0,
+          "rate": 0
+        }
+      }
+    },
+    "analytics": {
+      "username": true,
+      "burst": 1000,
+      "rate": 100,
+      "overrides": {
+        "${CAPI_ADMIN_LOGIN}": {
+          "burst": 0,
+          "rate": 0
+        }
+      }
+    }
+  }
+}
+HERE
+
+
+
+# v1
+cat > /opt/smartdc/v1/config/production.json <<HERE
+{
+  "name": "Joyent Public API",
+  "url": "http://${PUBLIC_IP}/v1",
+  "capi": {
+    "url": "${CAPI_URL}",
+    "username": "${CAPI_HTTP_ADMIN_USER}",
+    "password": "${CAPI_HTTP_ADMIN_PW}"
+  },
+  "mapi": {
+    "${DEFAULT_DATACENTER}": {
+      "url": "${MAPI_URL}",
+      "username": "${MAPI_HTTP_ADMIN_USER}",
+      "password": "${MAPI_HTTP_ADMIN_PW}",
+      "resources": {
+        "nodejs": {
+          "repo": true,
+          "domain": "*.no.de",
+          "ram": [128, 256, 512, 1024, 2048, 4096]
+        },
+        "smartos": {
+          "ram": [128, 256, 512, 1024, 2048, 4096]
+        }
+      }
+    }
   }
 }
 HERE
