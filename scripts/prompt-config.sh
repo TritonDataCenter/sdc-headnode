c08b50337202f9c9e0b5ef40f95abe036a99fa38
Jerry Jelinek <jerry.jelinek@joyent.com>
2012-05-29 11:05:10 -0600

    HEAD-1087 prompt-config using wrong values for zone IP addrs

--- scripts/prompt-config.sh
+++ scripts/prompt-config.sh
@@ -1037,7 +1037,7 @@ admin_network="$net_a.$net_b.$net_c.$net_d"
 #
 # Calculate admin network IP address for each core zone
 #
-next_addr=$(expr $next_addr + 1)
+next_addr=$admin_zone_ip
 assets_admin_ip="$net_a.$net_b.$net_c.$(expr $net_d + $next_addr)"
 
 next_addr=$(expr $next_addr + 1)
