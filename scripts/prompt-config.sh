99383c44a94adb0687d9edf87531912af738573d
Trent Mick <trent.mick@joyent.com>
2012-08-08 19:52:34 -0700

    IMGAPI-12 Amon probes for IMGAPI
    Also: get 'admin' user to use the 'mail_to' email addr provided in
    prompt-config for a useful email addr for Amon notifications.

--- scripts/prompt-config.sh
+++ scripts/prompt-config.sh
@@ -1347,7 +1347,7 @@ echo "ufds_ldap_root_dn=cn=root" >>$tmp_config
 echo "ufds_ldap_root_pw=secret" >>$tmp_config
 echo "ufds_admin_login=admin" >>$tmp_config
 echo "ufds_admin_pw=$zone_admin_pw" >>$tmp_config
-echo "ufds_admin_email=user@${domainname}" >>$tmp_config
+echo "ufds_admin_email=$mail_to" >>$tmp_config
 echo "ufds_admin_uuid=930896af-bf8c-48d4-885c-6573a94b1853" >>$tmp_config
 echo "# Legacy CAPI parameters" >>$tmp_config
 echo "capi_http_admin_user=admin" >>$tmp_config
