ed736904fcbd0813020082b8ccb880d908e80071
Josh Wilsdon <jwilsdon@joyent.com>
2012-10-10 23:04:39 -0700

    HEAD-1277 move workflow to being created just after ufds

--- scripts/headnode.sh
+++ scripts/headnode.sh
@@ -607,9 +607,9 @@ if [[ -z ${skip_zones} ]]; then
     create_zone moray
     create_zone redis
     create_zone ufds
+    create_zone workflow
     create_zone amon
     create_zone napi
-    create_zone workflow
     create_zone rabbitmq
     create_zone imgapi
     create_zone cnapi
