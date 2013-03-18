0343b3ab1662a21c31f5160937dd34ee5586b8b2
Matt Smillie <matt.smillie@joyent.com>
2013-03-18 16:50:32 -0700

    Revert "HEAD-1396 Add option for doing initial setup with images for the zones instead of tarballs"
    This reverts commit 72562cbf5024f69f512ed40c70d24da531cc5bd4.

--- scripts/build-payload.js
+++ scripts/build-payload.js
@@ -64,7 +64,7 @@ async.series([
                 }
                 obj.dataset_name = data.toString().split('\n')[0];
                 fs.readFile('/usbkey/datasets/' + obj.dataset_name
-                , function (err, data) {
+                    + '.dsmanifest', function (err, data) {
 
                     var dsmanifest;
 
