0c2833fddc0a38a96cfebbef51d81894240b526b
Josh Wilsdon <jwilsdon@joyent.com>
2013-09-17 17:34:05 -0700

    add missing zones to get-latest

--- incr-upgrade-scripts/get-latest.sh
+++ incr-upgrade-scripts/get-latest.sh
@@ -5,7 +5,7 @@
 # set -o errexit
 # set -o xtrace
 
-ROLES="adminui amon amonredis ca cloudapi cnapi dapi dhcpd fwapi imgapi napi sapi ufds usageapi vmapi workflow"
+ROLES="adminui amon amonredis assets ca cloudapi cnapi dapi dhcpd fwapi imgapi keyapi napi papi rabbitmq redis sapi sdc sdcsso ufds usageapi vmapi workflow"
 
 function print_latest
 {
@@ -18,3 +18,12 @@ function print_latest
 for ROLE in $ROLES; do
     print_latest $ROLE "$(updates-imgadm list version=~master name=$ROLE | tail -1)"
 done
+
+# manta-nameservice == binder
+echo "export BINDER_IMAGE=$(updates-imgadm list version=~master name=manta-nameservice | tail -1 | cut -d ' ' -f1)"
+# manta-postgres == manatee
+echo "export MANATEE_IMAGE=$(updates-imgadm list version=~master name=manta-postgres | tail -1 | cut -d ' ' -f1)"
+# manta-deployment == manta
+echo "export MANTA_IMAGE=$(updates-imgadm list version=~master name=manta-deployment | tail -1 | cut -d ' ' -f1)"
+# manta-moray == moray
+echo "export MORAY_IMAGE=$(updates-imgadm list version=~master name=manta-moray | tail -1 | cut -d ' ' -f1)"
