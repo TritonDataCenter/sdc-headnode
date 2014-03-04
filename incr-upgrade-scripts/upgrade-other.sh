274e53395f5cba6557222c98b5d9d285a62ba331
Trent Mick <trent.mick@joyent.com>
2014-03-03 22:56:42 -0800

    HEAD-1910: disable core zone maintain_resolvers update for now until have logic correct, fix papi SAPI metadata updates

--- incr-upgrade-scripts/upgrade-other.sh
+++ incr-upgrade-scripts/upgrade-other.sh
@@ -38,26 +38,31 @@ for data in $(sdc-sapi /services?application_uuid=$SDC_APP | json -H -a uuid nam
     fi
 done
 
-# 2. Update current core VMs. To work, this depends on ZAPI-472,
-#    https://mo.joyent.com/vmapi/commit/8f3a47d, which added 'update' workflow
-#    version 7.0.7. So we need at least that version.
-ufds_admin_uuid=$(bash /lib/sdc/config.sh -json | json ufds_admin_uuid)
-update_workflow_vers=$(sdc-workflow /workflows \
-    | json -Ha -c '/^update-[\d\.]+$/.test(this.name)' -e 'this.ver=this.name.split("-").slice(-1)[0]' ver | sort)
-# TODO: I'm not sure how to check semver *greater than or equal* to 7.0.7,
-#       so just checking for 7.0.7 presence.
-if [[ "$(echo "$update_workflow_vers" | grep '7\.0\.7' || true)" == "7.0.7" ]]; then
-    sdc-vmapi /vms?state=active\&owner_uuid=$ufds_admin_uuid \
-        | json -Ha uuid alias maintain_resolvers \
-        | while read uuid alias maintain_resolvers; do
-        if [[ "$maintain_resolvers" != "true" ]]; then
-            sdc-vmapi /vms/$uuid?action=update -X POST -d '{"maintain_resolvers": true}' \
-                | sdc sdc-waitforjob
-        fi
-    done
-else
-    echo "Skip HEAD-1910 upgrade until VMAPI upgraded with ZAPI-472."
-fi
+# TODO(HEAD-1910): only do the upgrade for core VMs that are on a platform
+# >=  20140212T195911Z.
+#
+## 2. Update current core VMs. To work, this depends on ZAPI-472,
+##    https://mo.joyent.com/vmapi/commit/8f3a47d, which added 'update' workflow
+##    version 7.0.7. So we need at least that version.
+#ufds_admin_uuid=$(bash /lib/sdc/config.sh -json | json ufds_admin_uuid)
+#update_workflow_vers=$(sdc-workflow /workflows \
+#    | json -Ha -c '/^update-[\d\.]+$/.test(this.name)' -e 'this.ver=this.name.split("-").slice(-1)[0]' ver | sort)
+## TODO: I'm not sure how to check semver *greater than or equal* to 7.0.7,
+##       so just checking for 7.0.7 presence.
+#if [[ "$(echo "$update_workflow_vers" | grep '7\.0\.7' || true)" == "7.0.7" ]]; then
+#    sdc-vmapi /vms?state=active\&owner_uuid=$ufds_admin_uuid \
+#        | json -Ha uuid alias maintain_resolvers \
+#        | while read uuid alias maintain_resolvers; do
+#        if [[ "$maintain_resolvers" != "true" ]]; then
+#            echo ""
+#            echo "Set maintain_resolvers=true on VM $uuid ($alias)."
+#            sdc-vmapi /vms/$uuid?action=update -X POST -d '{"maintain_resolvers": true}' \
+#                | sdc sdc-waitforjob
+#        fi
+#    done
+#else
+#    echo "Skip HEAD-1910 upgrade until VMAPI upgraded with ZAPI-472."
+#fi
 
 
 # -- HEAD-1916: SERVICE_DOMAIN on papi svc, PAPI_SERVICE/papi_domain on sdc app
@@ -66,12 +71,13 @@ SDC_APP=$(sdc-sapi /applications?name=sdc | json -H 0.uuid)
 DOMAIN=$(sdc-sapi /applications/$SDC_APP | json -H metadata.datacenter_name).$(sdc-sapi /applications/$SDC_APP | json -H metadata.dns_domain)
 papi_domain=papi.$DOMAIN
 
-papi_service=$(sdc-sapi /services?name=papi | json -Ha 0.uuid)
+sapi_url=$(sdc-sapi /applications/$SDC_APP | json -H metadata.sapi-url)
+papi_service=$(sdc-sapi /services?name=papi | json -H 0.uuid)
 if [[ -n "$papi_service" ]]; then
-    has_it=$(sdc-sapi /services/$papi_service | json -H metadata.SERVICE_DOMAIN)
-    if [[ -z "$has_it" ]]; then
-        sapiadm update $papi_service metadata.SERVICE_DOMAIN=$papi_domain
-    fi
+    echo ""
+    echo "Upgrade PAPI service vars in SAPI."
+    sapiadm update $papi_service metadata.SERVICE_DOMAIN=$papi_domain
+    sapiadm update $papi_service metadata.sapi-url=$sapi_url
     sapiadm update $SDC_APP metadata.PAPI_SERVICE=$papi_domain
     sapiadm update $SDC_APP metadata.papi_domain=$papi_domain
 fi
