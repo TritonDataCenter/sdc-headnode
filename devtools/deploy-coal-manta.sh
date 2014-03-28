34342ea631abee8655f75d7797a2dac3beddb3bb
Nate Fitch <nate.fitch@joyent.com>
2014-03-28 13:50:02 -0700

    MANTA-2099: Manta deployment fails with manta-shardadm: command not found

--- devtools/deploy-coal-manta.sh
+++ devtools/deploy-coal-manta.sh
@@ -15,6 +15,6 @@ cd /var/tmp/networking
 cd -
 
 # Now deploy...
-zlogin $MANTA "/opt/smartdc/manta-deployment/bin/manta-init -e nobody@joyent.com -s coal -r coal"
-zlogin $MANTA "/opt/smartdc/manta-deployment/bin/manta-deploy-coal"
+zlogin $MANTA "bash -l -c \"/opt/smartdc/manta-deployment/bin/manta-init -e nobody@joyent.com -s coal -r coal\""
+zlogin $MANTA "bash -l -c \"/opt/smartdc/manta-deployment/bin/manta-deploy-coal\""
 sdc-ldap add -f /zones/$MANTA/root/opt/smartdc/manta-deployment/ufds/devs.ldif
