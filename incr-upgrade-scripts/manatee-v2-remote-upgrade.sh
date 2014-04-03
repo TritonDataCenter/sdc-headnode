bddfc1ee1e7c4ba8fbfe301396e2ec8e3e8c336f
Matt Smillie <matt.smillie@joyent.com>
2014-04-03 10:37:41 -0700

    RFC-39 MANATEE-133 manatee flag-day upgrade scripts.

---
+++ incr-upgrade-scripts/manatee-v2-remote-upgrade.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/bash
+#
+# Copyright (c) 2014, Joyent, Inc. All rights reserved.
+#
+
+
+
+LOG_FILENAME=/tmp/manatee-v2-remote-upgrade.$$.log
+exec 4>${LOG_FILENAME}
+export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+export BASH_XTRACEFD=4
+PATH=/opt/smartdc/bin:/usr/sbin:/usr/bin:$PATH
+
+set -o errexit
+set -o xtrace
+set -o pipefail
+
+function fatal
+{
+    echo "FATAL: $*" >&2
+    exit 2
+}
+
+function usage
+{
+    cat >&2 <<EOF
+Usage: $0 <instance_uuid> <tarball_path>
+EOF
+    exit 1
+}
+
+function manatee_stat
+{
+    # manatee-stat exists in different places depending on the manatee version
+    local m_stat=
+    if [[ -f /zones/${manatee_instance}/root/opt/smartdc/manatee/bin/manatee-stat ]]; then
+        m_stat="/opt/smartdc/manatee/bin/manatee-stat"
+    elif [[ -f /zones/${manatee_instance}/root/opt/smartdc/manatee/node_modules/manatee/bin/manatee-stat ]]; then
+        m_stat="/opt/smartdc/manatee/node_modules/manatee/bin/manatee-stat -p \$ZK_IPS"
+    else
+        fatal "Can't find manatee-stat."
+    fi
+    local result=$(zlogin ${manatee_instance} "source /opt/smartdc/etc/zk_ips.sh; ${m_stat}");
+    echo ${result}
+}
+
+function wait_for_manatee
+{
+    local expect=$1
+    local result=
+    local count=0
+
+    while [[ ${result} != ${expect} ]]; do
+        result=$(manatee_stat | json -e '
+            if (Object.keys(this.sdc).length===0) {
+                this.mode = "empty";
+            } else if (this.sdc.primary && this.sdc.sync && this.sdc.async) {
+                var up = this.sdc.async.repl && !this.sdc.async.repl.length && Object.keys(this.sdc.async.repl).length === 0;
+                if (up && this.sdc.sync.repl && this.sdc.sync.repl.sync_state == "async") {
+                    this.mode = "async";
+                }
+            } else if (this.sdc.primary && this.sdc.sync) {
+                var up = this.sdc.sync.repl && !this.sdc.sync.repl.length && Object.keys(this.sdc.sync.repl).length === 0;
+                if (up && this.sdc.primary.repl && this.sdc.primary.repl.sync_state == "sync") {
+                    this.mode = "sync";
+                }
+            } else if (this.sdc.primary) {
+                var up = this.sdc.primary.repl && !this.sdc.primary.repl.length && Object.keys(this.sdc.primary.repl).length === 0;
+                if (up) {
+                    this.mode = "primary";
+                }
+            }
+
+            if (!this.mode) {
+                this.mode = "transition";
+            }' mode)
+        if [[ ${result} == ${expect} ]]; then
+            continue;
+        elif [[ ${count} -gt 24 ]]; then
+            fatal "Timeout (120s) waiting for manatee to reach ${target}"
+        else
+            count=$((${count} + 1))
+            sleep 5
+        fi
+    done
+}
+
+function crack_tarball
+{
+    local dest=/zones/${manatee_instance}/root/opt/smartdc
+    if [[ ! -d ${dest} ]]; then
+        fatal "No such destination: ${dest}"
+    fi
+    mkdir -p ${dest}/manatee-new
+    tar zxf ${tarball} -C ${dest}/manatee-new
+}
+
+function swap_code
+{
+    zlogin ${manatee_instance} mv /opt/smartdc/manatee /opt/smartdc/manatee-old
+    zlogin ${manatee_instance} mv /opt/smartdc/manatee-new /opt/smartdc/manatee
+}
+
+function ensure_correct_config
+{
+
+    svcadm -z ${manatee_instance} disable -s config-agent
+    zlogin ${manatee_instance} rm /opt/smartdc/manatee/etc/sitter.json
+    svcadm -z ${manatee_instance} enable -s config-agent
+
+    # wait until config appears
+    local count=0
+    while [[ ! -f /zones/${manatee_instance}/root/opt/smartdc/manatee/etc/sitter.json ]]; do
+        if [[ ${count} -gt 12 ]]; then
+            fatal "Timeout waiting for config in ${manatee_instance}"
+        else
+            count=$((${count} + 1))
+            sleep 5
+        fi
+    done
+}
+
+function import_smf
+{
+    zlogin ${manatee_instance} svccfg import /opt/smartdc/manatee/smf/manifests/sitter.xml
+}
+
+function touch_sync_state
+{
+    cookieLocation=$(zlogin ${manatee_instance} json -f /opt/smartdc/manatee/etc/sitter.json \
+                     postgresMgrCfg.syncStateCheckerCfg.cookieLocation)
+    if [[ ! -f ${cookieLocation} ]]; then
+        zlogin ${manatee_instance} touch ${cookieLocation}
+        zlogin ${manatee_instance} chown postgres:postgres ${cookieLocation}
+    fi
+}
+
+# seems unnecessary, but PATH variable in the smf manifest doesn't seem
+# to be respected in this situation?
+function node_version
+{
+    local old_node=/opt/local/bin/node
+    local new_node=/opt/smartdc/manatee/build/node/bin/node
+    zlogin ${manatee_instance} mv ${old_node} /opt/local/bin/node0.8
+    zlogin ${manatee_instance} ln -s ${new_node} ${old_node}
+}
+
+# if sitter up, get current state
+#   bring sitter down
+#   wait for state-1
+# endif
+# get current state
+# bring sitter up
+# wait for state+1
+function restart_sitter
+{
+    local state=$(svcs -Ho state -z ${manatee_instance} manatee-sitter)
+    local final_state=
+    if [[ ${state} == "online" ]]; then
+        # assume this is for single-node restart.
+        svcadm -z ${manatee_instance} disable -s manatee-sitter
+        wait_for_manatee empty
+        final_state="primary"
+    elif [[ ${state} == "disabled" ]]; then
+        final_state="sync"
+    else
+        fatal "Manatee ${manatee_instance} in unexpected state ${state}"
+    fi
+    svcadm -z ${manatee_instance} enable manatee-sitter
+    wait_for_manatee ${final_state}
+}
+
+# vars, params
+
+manatee_instance=$1
+if [[ -z ${manatee_instance} ]]; then
+    usage
+fi
+
+tarball=$2
+if [[ ! -f ${tarball} ]]; then
+    usage
+fi
+
+echo "!! log file is ${LOG_FILENAME}"
+
+# mainline
+
+crack_tarball
+swap_code
+ensure_correct_config
+import_smf
+touch_sync_state
+node_version
+restart_sitter
+
+echo "done."
+
---
+++ incr-upgrade-scripts/manatee-v2-upgrade.sh
@@ -0,0 +1,227 @@
+#!/usr/bin/bash
+#
+# Copyright (c) 2014, Joyent, Inc. All rights reserved.
+#
+
+
+
+LOG_FILENAME=/tmp/manatee-v2-upgrade.$$.log
+exec 4>${LOG_FILENAME}
+export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+export BASH_XTRACEFD=4
+PATH=/opt/smartdc/bin:/usr/sbin:/usr/bin:$PATH
+
+set -o errexit
+set -o xtrace
+set -o pipefail
+
+function fatal
+{
+    echo "FATAL: $*" >&2
+    exit 2
+}
+
+function usage
+{
+    cat >&2 <<EOF
+Usage: $0 <image_uuid>
+EOF
+    exit 1
+}
+
+# params
+image_uuid=$1
+if [[ -z ${image_uuid} ]]; then
+    usage
+fi
+
+function create_tarball
+{
+    tarball=/tmp/manatee-v2-upgrade.tgz
+    if [[ -f ${tarball} ]]; then
+        rm ${tarball}
+    fi
+    local image_uuid=$1
+    local source_path=/zones/${image_uuid}/root/opt/smartdc/manatee
+    pushd ${source_path}
+    tar zcf ${tarball} .
+    popd
+}
+
+local_manatee=$(vmadm lookup state=running tags.smartdc_role=manatee | tail -1)
+
+function manatee_stat
+{
+    # manatee-stat exists in different places depending on the manatee version,
+    # and therefore on the stage of the upgrade; find it explicitly.
+    local m_stat=
+    local result=
+    if [[ -f /zones/${local_manatee}/root/opt/smartdc/manatee/bin/manatee-stat ]]; then
+        m_stat="/opt/smartdc/manatee/bin/manatee-stat -p \$ZK_IPS"
+    elif [[ -f /zones/${local_manatee}/root/opt/smartdc/manatee/node_modules/manatee/bin/manatee-stat ]]; then
+        m_stat="/opt/smartdc/manatee/node_modules/manatee/bin/manatee-stat -p \$ZK_IPS"
+    else
+        fatal "Can't find manatee-stat."
+    fi
+    if [[ ! -f /zones/${local_manatee}/root/opt/smartdc/etc/zk_ips.sh ]]; then
+        # race condition on the creation of zk_ips when the local manatee
+        # is reprovisioning/rebooting.
+        result="{}"
+    else
+        result=$(zlogin ${local_manatee} "source /opt/smartdc/etc/zk_ips.sh; ${m_stat}");
+    fi
+    echo ${result}
+}
+
+function wait_for_manatee
+{
+    local expect=$1
+    local result=
+    local count=0
+    echo "Waiting for manatee to reach ${expect}"
+    while [[ ${result} != ${expect} ]]; do
+        result=$(manatee_stat | json -e '
+            if (!this.sdc) {
+                this.mode = "transition";
+            } else if (Object.keys(this.sdc).length===0) {
+                this.mode = "empty";
+            } else if (this.sdc.primary && this.sdc.sync && this.sdc.async) {
+                var up = this.sdc.async.repl && !this.sdc.async.repl.length && Object.keys(this.sdc.async.repl).length === 0;
+                if (up && this.sdc.sync.repl && this.sdc.sync.repl.sync_state == "async") {
+                    this.mode = "async";
+                }
+            } else if (this.sdc.primary && this.sdc.sync) {
+                var up = this.sdc.sync.repl && !this.sdc.sync.repl.length && Object.keys(this.sdc.sync.repl).length === 0;
+                if (up && this.sdc.primary.repl && this.sdc.primary.repl.sync_state == "sync") {
+                    this.mode = "sync";
+                }
+            } else if (this.sdc.primary) {
+                var up = this.sdc.primary.repl && !this.sdc.primary.repl.length && Object.keys(this.sdc.primary.repl).length === 0;
+                if (up) {
+                    this.mode = "primary";
+                }
+            }
+
+            if (!this.mode) {
+                this.mode = "transition";
+            }' mode)
+        if [[ ${result} == ${expect} ]]; then
+            continue;
+        elif [[ ${count} -gt 24 ]]; then
+            fatal "Timeout (120s) waiting for manatee to reach ${target}"
+        else
+            count=$((${count} + 1))
+            sleep 5
+        fi
+    done
+}
+
+function find_manatees
+{
+    primary_manatee=$(manatee_stat | json -Ha sdc.primary.zoneId)
+    if [[ -z ${primary_manatee} ]]; then
+        echo ""
+        fatal "Can't find primary manatee"
+    fi
+    primary_server=$(sdc-vmapi /vms/${primary_manatee} | json -Ha server_uuid)
+    if [[ $? != 0 || -z ${primary_server} ]]; then
+        echo ""
+        fatal "Can't find server for primary: ${primary_manatee}"
+    fi
+    sync_manatee=$(manatee_stat | json -Ha sdc.sync.zoneId)
+    if [[ -z ${sync_manatee} ]]; then
+        echo ""
+        fatal "Can't find sync manatee"
+    fi
+    sync_server=$(sdc-vmapi /vms/${sync_manatee} | json -Ha server_uuid)
+    if [[ $? != 0 || -z ${sync_server} ]]; then
+        echo ""
+        fatal "Can't find server for sync: ${sync_manatee}"
+    fi
+    async_manatee=$(manatee_stat | json -Ha sdc.async.zoneId)
+    if [[ -z ${async_manatee} ]]; then
+        echo ""
+        fatal "Can't find async manatee"
+    fi
+    async_server=$(sdc-vmapi /vms/${async_manatee} | json -Ha server_uuid)
+    if [[ $? != 0 || -z ${async_server} ]]; then
+        echo ""
+        fatal "Can't find server for async: ${async_manatee}"
+    fi
+}
+
+function disable_manatee
+{
+    local server=$1
+    local zone=$2
+    echo "Disabling manatee services in ${zone}"
+    sdc-oneachnode -n ${server} \
+    "svcadm -z ${zone} disable -s manatee-sitter;\
+     svcadm -z ${zone} disable -s manatee-snapshotter;\
+     svcadm -z ${zone} disable -s manatee-backupserver;"
+    if [[ $? != 0 ]]; then
+        echo ""
+        fatal "Couldn't disable manatee services in ${zone}"
+    fi
+}
+
+function reprovision_manatee
+{
+    local server=$1
+    local instance=$2
+    local current_image=$(sdc-vmapi /vms/${instance} -f | json image_uuid)
+
+    if [[ ${current_image} == ${image_uuid} ]]; then
+        echo "Manatee ${zone} already at image ${image_uuid}, skipping."
+    else
+        echo "Reprovisioning ${instance} to ${image_uuid}..."
+        sdc-oneachnode -n ${server} \
+            "imgadm import ${image_uuid}"
+        sdc-oneachnode -n ${server} \
+            "echo '{}' | json -e \"this.image_uuid='${image_uuid}'\" \
+            | vmadm reprovision ${instance}"
+    fi
+}
+
+# mainline
+
+echo "!! log file is ${LOG_FILENAME}"
+
+./download-image.sh ${image_uuid}
+
+echo "Creating upgrade tarball."
+create_tarball
+
+find_manatees
+disable_manatee ${async_server} ${async_manatee}
+wait_for_manatee sync
+disable_manatee ${sync_server} ${sync_manatee}
+wait_for_manatee primary
+
+echo "Upgrading primary manatee in place (1/5)"
+./manatee-v2-in-situ-upgrade.sh ${primary_server} ${primary_manatee} ${tarball}
+
+echo "Upgrading sync manatee in place (2/5)"
+./manatee-v2-in-situ-upgrade.sh ${sync_server} ${sync_manatee} ${tarball}
+
+# reprovision async
+echo "Reprovisioning async manatee (3/5)"
+disable_manatee ${async_server} ${async_manatee}
+wait_for_manatee sync
+reprovision_manatee ${async_server} ${async_manatee} ${image_uuid}
+wait_for_manatee async
+
+echo "Reprovisioning sync manatee (4/5)"
+disable_manatee ${sync_server} ${sync_manatee}
+wait_for_manatee sync
+reprovision_manatee ${sync_server} ${sync_manatee} ${image_uuid}
+wait_for_manatee async
+
+echo "Reprovisioning (previously) primary manatee (5/5)"
+disable_manatee ${primary_server} ${primary_manatee}
+wait_for_manatee sync
+reprovision_manatee ${primary_server} ${primary_manatee} ${image_uuid}
+wait_for_manatee async
+
+echo "Upgrade complete."
+
