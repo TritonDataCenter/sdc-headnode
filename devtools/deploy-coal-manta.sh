7b639a2603e82a29f703f811d306dd154457cb9b
Mark Cavage <mcavage@gmail.com>
2014-04-09 17:08:20 -0700

    MANTA-2123: deploy-coal-manta.sh should force marlin to use smartos-1.6.3

--- devtools/deploy-coal-manta.sh
+++ devtools/deploy-coal-manta.sh
@@ -17,6 +17,8 @@ if [[ ! -f /var/tmp/networking/configs/coal.json ]]; then
 fi
 
 # Now deploy...
+cat /opt/smartdc/agents/lib/node_modules/marlin/etc/agentconfig.coal.json | json -e 'zoneDefaultImage="1.6.3"' > /tmp/.agent.json
+mv /tmp/.agent.json /opt/smartdc/agents/lib/node_modules/marlin/etc/agentconfig.coal.json
 zlogin $MANTA "bash -l -c \"/opt/smartdc/manta-deployment/bin/manta-init -e nobody@joyent.com -s coal -r coal -m fd2cc906-8938-11e3-beab-4359c665ac99\""
 zlogin $MANTA "bash -l -c \"/opt/smartdc/manta-deployment/bin/manta-deploy-coal\""
 sdc-ldap add -f /zones/$MANTA/root/opt/smartdc/manta-deployment/ufds/devs.ldif
