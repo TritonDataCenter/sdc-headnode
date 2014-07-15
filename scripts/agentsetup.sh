cf12d8937a4b51573bcf572444d20d121aa92cad
Andres Rodriguez <andres@joyent.com>
2014-07-15 14:30:46 -0700

    AGENT-766: Import new agents SMF manifests when config-agent is ready

--- scripts/agentsetup.sh
+++ scripts/agentsetup.sh
@@ -204,6 +204,10 @@ EOF
 
     ${prefix}/build/node/bin/node ${prefix}/agent.js -s -f /opt/smartdc/agents/lib/node_modules/config-agent/etc/config.json
 
+    for agent in $CONFIGURABLE_AGENTS; do
+        svccfg import $AGENTS_DIR/smf/$agent.xml
+    done
+
     svccfg import $AGENTS_DIR/smf/config-agent.xml
     svcadm enable config-agent
 }
