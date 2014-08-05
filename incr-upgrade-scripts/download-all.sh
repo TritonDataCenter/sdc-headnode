98c26acc5299d7ef5c7c7104df1c95ce0dc58cab
Trent Mick <trent.mick@joyent.com>
2014-08-05 15:45:13 -0700

    HEAD-2143: assets zone in upgrade-all.sh, but not in download-all.sh

--- incr-upgrade-scripts/download-all.sh
+++ incr-upgrade-scripts/download-all.sh
@@ -42,3 +42,4 @@ source $IMAGE_LIST
 [[ -n "$RABBITMQ_IMAGE" ]] && ./download-image.sh $RABBITMQ_IMAGE
 [[ -n "$PAPI_IMAGE" ]] && ./download-image.sh $PAPI_IMAGE
 [[ -n "$MAHI_IMAGE" ]] && ./download-image.sh $MAHI_IMAGE
+[[ -n "$ASSETS_IMAGE" ]] && ./download-image.sh $ASSETS_IMAGE
