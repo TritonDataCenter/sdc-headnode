b466700dc4aeb42e6ffef289e1f9024756eaa47e
Rob Gulewich <robert.gulewich@joyent.com>
2014-10-01 12:43:28 -0700

    TOOLS-680 add_external_nic.sh can find an incorrect external network

--- scripts/add_external_nic.sh
+++ scripts/add_external_nic.sh
@@ -13,6 +13,8 @@
 # add_external_nics.sh add an external nic to a zone.
 #
 
+[[ -z "$1" ]] && echo "Usage: $(basename $0) <VM UUID>" && exit 1
+
 # BASHSTYLED
 export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -o xtrace
@@ -27,29 +29,18 @@ function fatal {
 function add_external_nic {
     local zone_uuid=$1
     local external_net_uuid
-    external_net_uuid=$(sdc-napi /networks?nic_tag=external | json -Ha uuid)
+    external_net_uuid=$(sdc-napi /networks?name=external | json -Ha uuid)
     local tmpfile=/tmp/update_nics.$$.json
 
-    local num_nics
-    num_nics=$(sdc-vmapi /vms/${zone_uuid} | json -H nics.length);
-
     echo "Adding external NIC to ${zone_uuid}"
-
-    echo "
-    {
-        \"networks\": [
-            {
-                \"uuid\": \"${external_net_uuid}\",
-                \"primary\": true
-            }
-        ]
-    }" > ${tmpfile}
-
-    sdc-vmapi /vms/${zone_uuid}?action=add_nics -X POST \
-        -d @${tmpfile}
+    sdc-vmapi /vms/${zone_uuid}?action=add_nics -X POST --data-binary @- <<EOF
+{
+  "networks": [
+    { "uuid": "${external_net_uuid}", "primary": true }
+  ]
+}
+EOF
     [[ $? -eq 0 ]] || fatal "failed to add external NIC"
-
-    rm -f ${tmpffile}
 }
 
 add_external_nic $1
