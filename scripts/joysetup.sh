5f06584d2b6282500e210155bae81928cc7d357c
Travis Paul <travis.paul@mnx.io>
2023-05-16 16:24:54 -0500

    LINUXCN-14 Restart triton-lxd service when setting up linuxcn

--- scripts/joysetup.sh
+++ scripts/joysetup.sh
@@ -755,6 +755,8 @@ setup_datasets()
             # Linux mounts to /var
             zfs set mountpoint=/var "${VARDS}" || \
             fatal "failed to set the mountpoint for ${VARDS}"
+            # Restart triton-lxd service so imgadm list succeeds later
+            systemctl restart triton-lxd.service
         fi
 
         zfs set atime=on "${VARDS}" || \
